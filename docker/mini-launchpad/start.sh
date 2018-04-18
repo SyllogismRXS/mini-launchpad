eb90986cd1180563b77e3fb1fa50a6f0047bf135
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2018-04-17 23:59:49 -0400

    configuring docker-compose

--- docker/mini-launchpad/start.sh
+++ docker/mini-launchpad/start.sh
@@ -7,9 +7,11 @@ declare -a arch_array=("amd64" "arm64" "armhf" "i386")
 for arch in "${arch_array[@]}"
 do
     if [ ! -f /var/cache/pbuilder/${dist}-${arch}-base.tgz ]; then
-        DIST=${dist} ARCH=${arch} pbuilder --create
+        DIST=${dist} ARCH=${arch} pbuilder --create \
+            --configfile=/root/.pbuilderrc
     else
-        DIST=${dist} ARCH=${arch} pbuilder --update
+        DIST=${dist} ARCH=${arch} pbuilder --update \
+            --configfile=/root/.pbuilderrc
     fi
 done
 
@@ -17,4 +19,9 @@ echo "============================================="
 echo "pbuilder environment configuration complete"
 echo "============================================="
 
-/usr/bin/mini-launchpad
+/usr/bin/mini-launchpad --dist xenial \
+                        --arch amd64 armhf i386 arm64 \
+                        --ftp-loc /root/incoming \
+                        --pbuilder-config /root/.pbuilderrc \
+                        --dput-config /root/.dput.cf \
+                        --dput-name local-ftp \

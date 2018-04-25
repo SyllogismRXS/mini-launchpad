efbb7d3c1e6d2605f38a45927f0f679d0a3bdcac
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2018-04-25 12:03:26 -0400

    Updated pbuilderrc

--- docker/mini-launchpad/start.sh
+++ docker/mini-launchpad/start.sh
@@ -6,6 +6,8 @@ declare -a arch_array=("amd64" "arm64" "armhf" "i386")
 
 for arch in "${arch_array[@]}"
 do
+    arch_str="${arch_str} ${arch}"
+
     if [ ! -f /var/cache/pbuilder/${dist}-${arch}-base.tgz ]; then
         DIST=${dist} ARCH=${arch} pbuilder --create \
             --configfile /root/.pbuilderrc
@@ -20,7 +22,7 @@ echo "pbuilder environment configuration complete"
 echo "============================================="
 
 /usr/bin/mini-launchpad --dist xenial \
-                        --arch amd64 armhf i386 arm64 \
+                        --arch "${arch_str}" \
                         --ftp-loc /root/incoming \
                         --pbuilder-config /root/.pbuilderrc \
                         --dput-config /root/.dput.cf \

7d60bf2ec2f258c85b6899855a390eb31875f7bd
Nathalie Ruano <nruano@gatech.edu>
2018-05-01 22:13:18 -0400

    Added mutex lock and checks for only one package build at a time

--- docker/mini-launchpad/start.sh
+++ docker/mini-launchpad/start.sh
@@ -2,29 +2,31 @@
 
 dist="xenial"
 
-declare -a arch_array=("amd64" "arm64" "armhf" "i386")
+#declare -a arch_array=("amd64" "arm64" "armhf" "i386")
+declare -a arch_array=("amd64")
 
-for arch in "${arch_array[@]}"
-do
-    arch_str="${arch_str} ${arch}"
+#for arch in "${arch_array[@]}"
+#do
+#   arch_str="${arch_str} ${arch}"
 
-    if [ ! -f /var/cache/pbuilder/${dist}-${arch}-base.tgz ]; then
-        DIST=${dist} ARCH=${arch} pbuilder --create \
-            --configfile /root/.pbuilderrc
-    else
-        DIST=${dist} ARCH=${arch} pbuilder --update \
-            --configfile /root/.pbuilderrc
-    fi
-done
+#   if [ ! -f /var/cache/pbuilder/${dist}-${arch}-base.tgz ]; then
+#       DIST=${dist} ARCH=${arch} pbuilder --create \
+#           --configfile /home/gtri-uav/.pbuilderrc
+#   else
+#       DIST=${dist} ARCH=${arch} pbuilder --update \
+#           --configfile /home/gtri-uav/.pbuilderrc
+#   fi
+#done
 
 echo "============================================="
 echo "pbuilder environment configuration complete"
 echo "============================================="
 
-/usr/bin/mini-launchpad --dist xenial \
-                        --arch ${arch_str} \
-                        --ftp-incoming /root/incoming \
-                        --pbuilder-config /root/.pbuilderrc \
-                        --dput-config /root/.dput.cf \
-                        --dput-name local-ftp \
-                        --log-path /root/build-logs
+python -u /home/gtri-uav/mini-launchpad/mini-launchpad/mini-launchpad --dist xenial \
+                                  --arch ${arch_str} \
+                                  --ftp-incoming /home/gtri-uav/test \
+				  --ftp-incoming-clear true \
+                                  --pbuilder-config /home/gtri-uav/.pbuilderrc \
+                                  --dput-config /home/gtri-uav/.dput.cf \
+                                  --dput-name local \
+                                  --log-path /home/gtri-uav/build-logs

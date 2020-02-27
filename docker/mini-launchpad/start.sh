42dfd9f02687c5e6b4f7065fdc8d1906e853e2e2
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2020-02-27 15:25:03 +0000

    support xenial and bionic in docker

--- docker/mini-launchpad/start.sh
+++ docker/mini-launchpad/start.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-dist="xenial"
-
+declare -a dist_array=("xenial" "bionic")
 declare -a arch_array=("amd64" "arm64" "armhf" "i386")
 
+for dist in "${dist_array[@]}"
+do
+    for arch in "${arch_array[@]}"
+    do
+	arch_str="${arch_str} ${arch}"
+
+	if [ ! -f /var/cache/pbuilder/${dist}-${arch}-base.tgz ]; then
+            DIST=${dist} ARCH=${arch} pbuilder --create \
+		--configfile /root/.pbuilderrc
+	else
+            DIST=${dist} ARCH=${arch} pbuilder --update \
+		--configfile /root/.pbuilderrc --override-config
+	fi
+    done
+done
+
+# Build the dist_str
+for dist in "${dist_array[@]}"
+do
+    dist_str="${dist_str} ${dist}"
+done
+
+# Build the arch_str
 for arch in "${arch_array[@]}"
 do
     arch_str="${arch_str} ${arch}"
-
-    if [ ! -f /var/cache/pbuilder/${dist}-${arch}-base.tgz ]; then
-        DIST=${dist} ARCH=${arch} pbuilder --create \
-            --configfile /root/.pbuilderrc
-    else
-        DIST=${dist} ARCH=${arch} pbuilder --update \
-            --configfile /root/.pbuilderrc --override-config
-    fi
 done
 
 echo "============================================="
 echo "pbuilder environment configuration complete"
 echo "============================================="
 
-python -u /usr/bin/mini-launchpad --dist xenial \
+python -u /usr/bin/mini-launchpad --dist ${dist_str} \
                                   --arch ${arch_str} \
                                   --ftp-incoming /root/incoming \
                                   --pbuilder-config /root/.pbuilderrc \
